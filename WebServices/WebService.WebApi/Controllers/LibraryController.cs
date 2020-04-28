d54db8887c51fa64289d27d59e5a3ad6ec0e2bc4
Jorge Chan <jorgeluis-chan@hotmail.com>
2020-04-28 17:08:23 -0500

    Se crea controlador LibraryController y se agrega los metodos para listar todos los libros o listar un solo libro.

---
+++ WebServices/WebService.WebApi/Controllers/LibraryController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebService.Data.Models;
+
+namespace WebService.WebApi.Controllers
+{
+    public class LibraryController : ApiController
+    {
+        LibraryConnection ConnectionDB = new LibraryConnection();
+
+        /// <summary>
+        /// Allows to consult the information of all books
+        /// </summary>
+        /// <returns></returns>    
+        [HttpGet]
+        public IEnumerable<Libros> Get()
+        {
+            var List = ConnectionDB.Libros.ToList();
+            return List;
+        }
+
+        /// <summary>
+        /// Allows to consult the information of a book
+        /// </summary>
+        /// <returns></returns>    
+        [HttpGet]
+        public Libros Get(int Id)
+        {
+            var Libros = ConnectionDB.Libros.FirstOrDefault(x => x.IdLibro == Id);
+            return Libros;
+        }
+    }
+}
