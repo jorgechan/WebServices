95096e94a5ab722f79c7e0ca8c6b229e48ede204
Jorge Chan <jorgeluis-chan@hotmail.com>
2020-04-29 09:37:24 -0500

    Se agrega al proyecto WebService.ASMX: - Referencia de EntityFramework con Nuget - Cadena de conexion al Web.config - Metodo para obtener listado de libros a la clase LibraryService

---
+++ WebServices/WebService.ASMX/LibraryService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="LibraryService.asmx.cs" Class="WebService.ASMX.LibraryService" %>
---
+++ WebServices/WebService.ASMX/LibraryService.asmx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using WebService.Data.Models;
+
+namespace WebService.ASMX
+{
+    /// <summary>
+    /// Descripción breve de LibraryService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+
+    public class LibraryService : System.Web.Services.WebService
+    {
+        LibraryConnection ConnectionDB = new LibraryConnection();
+
+        [WebMethod]
+        public List<Libros> GetBooks()
+        {
+            var List = ConnectionDB.Libros.ToList();
+            return List;
+        }
+    }
+}
