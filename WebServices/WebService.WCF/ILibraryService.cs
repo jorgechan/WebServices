e20ab14fea3f69e6bebf38dbf2f7e203da7ca4bc
Jorge Chan <jorgeluis-chan@hotmail.com>
2020-04-29 18:02:28 -0500

    A WebService.WCF: - Se cambio el nombre de IService1 a ILibraryService - Se cambio  el nombre de Service1 a LibraryService -Se agrega metodo para obtener listado de libros - Se agrega referencia de EntityFramework con Nuget - Se agrega cadena de conexion a la base de datos

---
+++ WebServices/WebService.WCF/ILibraryService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using WebService.Data.Models;
+
+namespace WebService.WCF
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface ILibraryService
+    {
+
+        [OperationContract]
+        List<Libros> GetBooks();
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+}
--- WebServices/WebService.WCF/LibraryService.svc.cs
+++ WebServices/WebService.WCF/LibraryService.svc.cs
@@ -5,29 +5,20 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using WebService.Data.Models;
 
 namespace WebService.WCF
 {
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
     // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
-    public class Service1 : IService1
+    public class LibraryService : ILibraryService
     {
-        public string GetData(int value)
-        {
-            return string.Format("You entered: {0}", value);
-        }
+        LibraryConnection ConnectionDB = new LibraryConnection();
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public List<Libros> GetBooks()
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            var List = ConnectionDB.Libros.ToList();
+            return List;
         }
     }
 }
