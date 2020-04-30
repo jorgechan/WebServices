1063693fc1e09cfddca5f2f57c2704d71a4cf33f
Jorge Chan <jorgeluis-chan@hotmail.com>
2020-04-30 18:39:59 -0500

    Se consume servicio web REST y se agrega referencia de newtonsoft.json a WebService.Consoleclient
    Se agrega respaldo de la base de datos de Library

--- WebServices/WebService.ConsoleClient/Program.cs
+++ WebServices/WebService.ConsoleClient/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WebService.Data.Models;
 
 namespace WebService.ConsoleClient
 {
@@ -10,12 +13,38 @@ namespace WebService.ConsoleClient
     {
         static void Main(string[] args)
         {
-            // Consume asmx web service
-            LibraryServiceASMX.LibraryServiceSoapClient ClientLibrary = new LibraryServiceASMX.LibraryServiceSoapClient();
-            var ListBooks = ClientLibrary.GetBooks();
+            //// Consume ASMX web service
+            //LibraryServiceASMX.LibraryServiceSoapClient ClientLibrary = new LibraryServiceASMX.LibraryServiceSoapClient();
+            //var ListBooks = ClientLibrary.GetBooks();
 
-            LibraryServiceWCF.LibraryServiceClient ClientLibrary1 = new LibraryServiceWCF.LibraryServiceClient();
-            var ListBooks1 = ClientLibrary1.GetBooks();
+            //// Consume WCF web service
+            //LibraryServiceWCF.LibraryServiceClient ClientLibrary1 = new LibraryServiceWCF.LibraryServiceClient();
+            //var ListBooks1 = ClientLibrary1.GetBooks();
+
+            // Consume REST web service
+            HttpClient ClientHttp = new HttpClient();
+
+            // We establish a site where each of the methods will be invoked
+            ClientHttp.BaseAddress = new Uri("http://localhost:53424/");
+
+            // We indicate that it is invoked by the get method starting from the indicated site
+            var Request = ClientHttp.GetAsync("api/Library").Result;
+
+            // We validate if the request was successful
+            if (Request.IsSuccessStatusCode)
+            {
+                // Map the result obtained from a request
+                var ResultString = Request.Content.ReadAsStringAsync().Result;
+
+                // Convert json result to class
+                var ListBooks = JsonConvert.DeserializeObject<List<Libros>>(ResultString);
+
+                // Print a book list
+                foreach (var iListBooks in ListBooks)
+                {
+                    Console.WriteLine(iListBooks.Titulo);
+                }
+            }
         }
     }
 }
