90bcaeda0dbd1b333fd4b733501cd93b62ae136e
Jorge Chan <jorgeluis-chan@hotmail.com>
2020-04-30 14:26:01 -0500

    - Se agrega referencias de servicios a WebService.ConsoleClient - Se invocan los servicios web ASMX y WCF

--- WebServices/WebService.ConsoleClient/Program.cs
+++ WebServices/WebService.ConsoleClient/Program.cs
@@ -10,6 +10,12 @@ namespace WebService.ConsoleClient
     {
         static void Main(string[] args)
         {
+            // Consume asmx web service
+            LibraryServiceASMX.LibraryServiceSoapClient ClientLibrary = new LibraryServiceASMX.LibraryServiceSoapClient();
+            var ListBooks = ClientLibrary.GetBooks();
+
+            LibraryServiceWCF.LibraryServiceClient ClientLibrary1 = new LibraryServiceWCF.LibraryServiceClient();
+            var ListBooks1 = ClientLibrary1.GetBooks();
         }
     }
 }
